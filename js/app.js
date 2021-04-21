bae7fc771269e9f750268a80dc501a54062a5053
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-04-21 13:40:37 -0500

    add javascript

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-const oneContainer = document.querySelectorAll('.slider__container');
+const globalContainer = document.getElementById('slider');
+const firstContainer = document.getElementById('first');
+const secondContainer = document.getElementById('second');
+const thirdContainer = document.getElementById('third');
 
-console.log(oneContainer);
+console.log(globalContainer);
 
-/* function changeContainer() {
-   let numInterval = setInterval(addClass, 1000);
+function changeContainer() {
+   setTimeout( modifyContainer => addClass(firstContainer) , 3000 );
+   setTimeout( modifyContainer => removeClass(secondContainer) , 3001 );
+   setTimeout( modifyContainer => addClass(secondContainer) , 6000 );
+   setTimeout( modifyContainer => removeClass(thirdContainer) , 6001 );
+   return console.log('ya cargó el dom perrín');
 }
 
-let addClass = agregarClase => console.log('hola hola') */
+let addClass = containerOn => containerOn.classList.add("off");
+let removeClass = containerOff => containerOff.classList.remove("off");
