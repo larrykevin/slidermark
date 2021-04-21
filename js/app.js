b0473e111263c82b8b48bb497dfb091eef738a5a
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-04-21 12:22:10 -0500

    agregando estilos

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const oneContainer = document.querySelectorAll('.slider__container');
+
+console.log(oneContainer);
+
+/* function changeContainer() {
+   let numInterval = setInterval(addClass, 1000);
+}
+
+let addClass = agregarClase => console.log('hola hola') */
